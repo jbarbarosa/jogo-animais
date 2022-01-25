e78ffe78499343450bfcd6f07cfa2e440ce374bc
me <code.e0ngu@simplelogin.co>
2022-01-24 21:31:16 -0300

    init

---
+++ src/cmd/Main.java
@@ -0,0 +1,7 @@
+package cmd;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/tree/AnimalTree.java
@@ -0,0 +1,11 @@
+package tree;
+
+public class AnimalTree {
+
+    private Node root;
+
+    public AnimalTree() {
+        this.root = new Node("O animal que você pensou vive na terra?");
+    }
+
+}
