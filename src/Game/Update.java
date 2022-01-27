2bc1dbcf8bc8388867968ac20a36a300b1620a53
me <code.e0ngu@simplelogin.co>
2022-01-26 22:21:51 -0300

    concluido

--- src/Game/Update.java
+++ src/Game/Update.java
@@ -7,14 +7,32 @@ import java.util.ArrayList;
 public class Update {
 
     public static void updateNode(Node node, ArrayList<String> updateWith) {
-        Node parent = node.getPrevious(); //vive na terra
-        Node clue = new Node(updateWith.get(0)); //voa
-        Node animal = new Node(updateWith.get(1)); //aguia
-        clue.setRight(animal); // voa -> s -> aguia
+        Node parent = node.getPrevious();
+        Node clue = new Node(updateWith.get(0));
+        Node animal = new Node(updateWith.get(1));
+        clue.setRight(animal);
         animal.setPrevious(clue);
         node.setPrevious(clue);
-        clue.setLeft(node); // voa -> n -> macaco
-        parent.setLeft(clue); //vive na terra -> n ->
+        clue.setLeft(node);
         clue.setPrevious(parent);
+        invertClueAndLeaf(parent, node, clue);
+    }
+
+    private static void invertClueAndLeaf(Node parent, Node leaf, Node clue) {
+        boolean res = isNodeParentsLeftChildren(parent, leaf);
+        if(res) {
+            parent.setLeft(clue);
+        } else {
+            parent.setRight(clue);
+        }
+    }
+
+    private static boolean isNodeParentsLeftChildren(Node parent, Node node) {
+        System.out.println("parent "+parent.getValue());
+        String parentLeftValue = parent.getLeft().getValue();
+        System.out.println("parent left: "+parentLeftValue);
+        String nodeValue = node.getValue();
+        System.out.println("node: "+nodeValue);
+        return parentLeftValue.equals(nodeValue);
     }
 }
